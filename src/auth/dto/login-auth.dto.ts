5cb73f764d634ef33629518d21a3d128a75a57f6
adilet-wd <usenkanov.adilet.kanatbekovich@gmail.com>
2023-12-16 14:19:31 +0600

    Завершение restAPI для AUTH

--- src/auth/dto/login-auth.dto.ts
+++ src/auth/dto/login-auth.dto.ts
@@ -1,11 +1,13 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsString } from "class-validator";
+import { IsNotEmpty, IsString } from "class-validator";
 
 export class LoginUserDto {
-    @ApiProperty({example: "user@gmail.com", description: "Уникальный почтовый адрес"})
+    @ApiProperty({example: "user@gmail.com", description: "Почтовый адрес пользователя"})
+    @IsNotEmpty({ message: 'Почтовый адрес не должен быть пустым' })
     @IsString({message: "Должно быть строкой"})
     readonly email: string;
-    @ApiProperty({example: "Pu0d1T9X", description: "Пароль"})
+    @ApiProperty({example: "Pu0d1T9X", description: "Пароль пользователя"})
+    @IsNotEmpty({ message: 'Пароль не должен быть пустым' })
     @IsString({message: "Должно быть строкой"})
     readonly password: string;
 }
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsEmail, IsEnum, IsString, Length} from "class-validator";
+import { IsEmail, IsEnum, IsString, Length, Matches, MaxLength} from "class-validator";
 
 enum UserRole {
     EMPLOYEE = 'EMPLOYEE',
@@ -9,14 +9,18 @@ enum UserRole {
 
 export class CreateUserDTO {
     
-    @ApiProperty({example: "Бексултан", description: "Имя. Не меньше 1 символа и не длинее 16"})
+    @ApiProperty({example: "Бексултан", description: "Имя. Не меньше 1 символа и не длинее 16. Имя должно быть на латинице."})
     @IsString({message: "Должно быть строкой"})
+    @Matches(/^[^0-9]+$/, { message: 'В имени не должно быть цифр'})
+    @Matches(/^[a-zA-Z]+$/, { message: 'Имя должно быть на латинице' })
     @Length(1, 16, {message: "Не меньше 1 символа и не длинее 16"})
     readonly name: string;
     
-    @ApiProperty({example: "Касымбеков", description: "Фамилия. Не меньше 1 символа и не длинее 16"})
+    @ApiProperty({example: "Касымбеков", description: "Фамилия. Не меньше 1 символа и не длинее 20. Фамилия должна быть на латинице."})
     @IsString({message: "Должно быть строкой"})
-    @Length(1, 16, {message: "Не меньше 1 символа и не длинее 16"})
+    @Matches(/^[^0-9]+$/, { message: 'В фамилии не должно быть цифр'})
+    @Matches(/^[a-zA-Z]+$/, { message: 'Фамилия должна быть на латинице.' })
+    @Length(1, 16, {message: "Не меньше 1 символа и не длинее 20"})
     readonly username: string;
 
     @ApiProperty({example: "user@gmail.com", description: "Уникальный почтовый адрес"})
@@ -24,9 +28,10 @@ export class CreateUserDTO {
     @IsEmail({}, {message: "Некорректный email"})
     readonly email: string;
 
-    @ApiProperty({example: "Pu0d1T9X", description: "Пароль. Не меньше 4 символов и не длинее 16"})
+    @ApiProperty({example: "Pu0d1T9X", description: "Пароль. Пароль должен содержать хотя бы 1 строчную букву, 1 заглавную букву, 1 цифры и быть не менее 8 символов длиной. Пароль должен быть не более 30 символов."})
     @IsString({message: "Должно быть строкой"})
-    @Length(4, 16, {message: "Не меньше 4 символов и не длинее 16"})
+    @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/, { message: 'Пароль должен содержать хотя бы 1 строчную букву, 1 заглавную букву, 1 цифры и быть не менее 8 символов длиной' })
+    @MaxLength(30, { message: 'Пароль должен быть не более 30 символов.'})
     readonly password: string;
 
     @ApiProperty({example: "EMPLOYEE", description: "Работник. Должно быть либо EMPLOYEE либо EMPLOYER"})
